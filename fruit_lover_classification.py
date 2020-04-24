491ef9aa72860b6f38353eb1bd43441ec9c94870
Erick Brockes <ebrockes@gmail.com>
2020-04-23 23:49:46 -0300

    Apply other algorithms

---
+++ fruit_lover_classification.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+
+#### color score
+# red - 0.85 to 1.00
+# orange - 0.75 to 0.85
+# yellow - 0.65 to 0.75
+# green - 0.45 to 0.65
+# ...
+
+fruits = pd.read_csv('fruit_data_with_colors.txt')
+print(fruits.head())
+
+lookup_fruit_name = dict(zip(fruits.fruit_label.unique(), fruits.fruit_name.unique()))
+print(lookup_fruit_name)
+
+X = fruits[['mass','width','height','color_score']]
+y = fruits['fruit_label']
+
+#feature space
+fruits.drop('fruit_label', axis=1).plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False, figsize=(9,9), title='Box Plot for each input variable')
+plt.savefig('fruits_box')
+plt.show()
+
+# relations - features
+cmap = cm.get_cmap('gnuplot')
+scatter = pd.plotting.scatter_matrix(X, c = y, marker = 'o', s=40, hist_kwds={'bins':15}, figsize=(9,9), cmap = cmap)
+plt.suptitle('Scatter-matrix for each input variable')
+plt.savefig('fruits_scatter_matrix')
+plt.show()
+
+
+#75 25 partitioning
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# 3 dimensional plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X_train['width'], X_train['height'], X_train['color_score'], c = y_train, marker = 'o', s=100)
+ax.set_xlabel('width')
+ax.set_ylabel('height')
+ax.set_zlabel('color_score')
+plt.show()
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+print('Accuracy of Logistic regression classifier on training set: {:.2f}'.format(logreg.score(X_train, y_train)))
+print('Accuracy of Logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+
+clf = DecisionTreeClassifier().fit(X_train, y_train)
+print('Accuracy of Decision Tree classifier on training set: {:.2f}'.format(clf.score(X_train, y_train)))
+print('Accuracy of Decision Tree classifier on test set: {:.2f}'.format(clf.score(X_test, y_test)))
+
+#knn = KNeighborsClassifier(n_neighbors = 5)
+knn = KNeighborsClassifier(algorithm = 'auto', leaf_size=10, metric='minkowski', metric_params=None, n_jobs=1, n_neighbors=5, p=2, weights='uniform')
+knn.fit(X_train, y_train)
+print('Accuracy of K-NN classifier on training set: {:.2f}'.format(knn.score(X_train, y_train)))
+print('Accuracy of K-NN classifier on test set: {:.2f}'.format(knn.score(X_test, y_test)))
+
+lda = LinearDiscriminantAnalysis()
+lda.fit(X_train, y_train)
+print('Accuracy of LDA classifier on training set: {:.2f}'.format(lda.score(X_train, y_train)))
+print('Accuracy of LDA classifier on test set: {:.2f}'.format(lda.score(X_test, y_test)))
+
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+print('Accuracy of GNB classifier on training set: {:.2f}'.format(gnb.score(X_train, y_train)))
+print('Accuracy of GNB classifier on test set: {:.2f}'.format(gnb.score(X_test, y_test)))
+
+svm = SVC()
+svm.fit(X_train, y_train)
+print('Accuracy of SVM classifier on training set: {:.2f}'.format(svm.score(X_train, y_train)))
+print('Accuracy of SVM classifier on test set: {:.2f}'.format(svm.score(X_test, y_test)))
+
+# For this particular dateset, we obtain the highest accuracy when k=5.
+k_range = range(1, 20)
+scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors = k)
+    knn.fit(X_train, y_train)
+    scores.append(knn.score(X_test, y_test))
+plt.figure()
+plt.xlabel('k')
+plt.ylabel('accuracy')
+plt.scatter(k_range, scores)
+plt.xticks([0,5,10,15,20])
+plt.show()
+
+print("############ Preditions for K-NN")
+#Use the trained k-NN classifier model to classify new, previously unseen objects
+fruit_prediction = knn.predict([[20, 4.3, 5.5, 0.9]])
+print('[20, 4.3, 5.5, 0.9]: ' + lookup_fruit_name[fruit_prediction[0]])
+fruit_prediction = knn.predict([[100, 6.3, 8.5, 0.1]])
+print('[100, 6.3, 8.5, 0.1]: ' + lookup_fruit_name[fruit_prediction[0]])
